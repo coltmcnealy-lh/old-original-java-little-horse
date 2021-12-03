67e049cc5cbbb89b1441debf2a63b8edbfa055dd
Colt McNealy <coltm@me.com>
2021-12-03 10:41:10 -0600

    cleanup

--- app/src/main/java/little/horse/lib/WFSpec/kafkaStreamsSerdes/WFSpecDeSerializer.java
+++ app/src/main/java/little/horse/lib/WFSpec/kafkaStreamsSerdes/WFSpecDeSerializer.java
@@ -2,19 +2,13 @@ package little.horse.lib.WFSpec.kafkaStreamsSerdes;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import org.apache.kafka.common.serialization.Deserializer;
 
 import little.horse.lib.WFSpec.WFSpecSchema;
 
 public class WFSpecDeSerializer implements Deserializer<WFSpecSchema> {
-  private Gson gson =
-      new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
 
   @Override
   public WFSpecSchema deserialize(String topic, byte[] bytes) {
