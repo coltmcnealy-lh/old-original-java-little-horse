449897dabd17a26eadd874e90797dad2d33a06b4
Little Horse McNealy <colt@littlehorse.io>
2022-03-24 18:32:22 -0500

    removes old examples.

--- examples/dummy_tasks/email.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-
-if __name__ == '__main__':
-    print("Sending fake email to " + sys.argv[1])
--- examples/dummy_tasks/get_random_character.py
+++
@@ -1,7 +0,0 @@
-import random
-
-
-if __name__ == '__main__':
-    print(random.choice({
-        "obiwan", "ahsoka", "palpatine", "vader", "yoda", "dooku"
-    }))
