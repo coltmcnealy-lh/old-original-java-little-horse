cc70a27e868785f51cec6e72c5b74bd12159b073
Little Horse McNealy <colt@littlehorse.io>
2022-04-08 13:48:27 -0500

    makes Kafka super replicated

--- kube/setup.sh
+++ kube/setup.sh
@@ -9,15 +9,22 @@ kind create cluster --name littlehorse --config ${SCRIPT_DIR}/kind-config.yaml
 
 kubectl apply -f ${SCRIPT_DIR}/kafka-ns.yaml
 kubectl apply -f ${SCRIPT_DIR}/strimzi-crd.yaml
-kubectl apply -f ${SCRIPT_DIR}/lh-kafka.yaml
+
+docker pull quay.io/strimzi/kafka:0.28.0-kafka-3.1.0 &
+docker pull quay.io/strimzi/operator:0.28.0 &
+
+wait
 
 kind load docker-image --name littlehorse quay.io/strimzi/kafka:0.28.0-kafka-3.1.0 &
-kind load docker-image --name littlehorse quay.io/strimzi/operator:0.28.0
+kind load docker-image --name littlehorse quay.io/strimzi/operator:0.28.0 &
 kind load docker-image --name littlehorse little-horse-api &
+
+wait
+
+kubectl apply -f ${SCRIPT_DIR}/lh-kafka.yaml
 
 kubectl wait kafka/lh-kafka --for=condition=Ready --timeout=300s -n kafka 
 
-wait
 
 kubectl apply -f ${SCRIPT_DIR}/core-api-0.yaml
 sleep 2
