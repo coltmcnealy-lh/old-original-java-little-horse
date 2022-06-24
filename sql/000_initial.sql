47719939722806d9eb2628f5ef369b9b6c28e151
Little Horse McNealy <colt@littlehorse.io>
2022-06-24 07:58:53 -0500

    some progress on sql but not much

--- sql/000_initial.sql
+++ sql/000_initial.sql
@@ -3,44 +3,80 @@ CREATE TABLE wf_specs (
     created TIMESTAMP NOT NULL DEFAULT NOW(),
     updated TIMESTAMP,
     name VARCHAR(256),
-    entrypoint_thread_name VARCHAR(256)
+    -- entrypoint_thread_name VARCHAR(256),
+
+    spec JSON
 );
 
-CREATE TABLE thread_specs (
-    id VARCHAR(256) PRIMARY KEY,
-    created TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated TIMESTAMP,
-    name VARCHAR(256),
-    wf_spec_id VARCHAR(256) REFERENCES wf_specs(id),
-    entrypoint_node_name VARCHAR(256)
-);
+-- CREATE TABLE thread_specs (
+--     id VARCHAR(256) PRIMARY KEY,
+--     created TIMESTAMP NOT NULL DEFAULT NOW(),
+--     updated TIMESTAMP,
+--     name VARCHAR(256),
+--     wf_spec_id VARCHAR(256) REFERENCES wf_specs(id),
+--     entrypoint_node_name VARCHAR(256),
 
-CREATE TYPE node_type_enum AS ENUM (
-    'TASK',
-    'EXTERNAL_EVENT',
-    'SPAWN_THREAD',
-    'WAIT_FOR_THREAD',
-    'SLEEP',
-    'NOP',
-    'THROW_EXCEPTION'
-);
+--     spec JSON
+-- );
+
+-- CREATE TYPE node_type_enum AS ENUM (
+--     'TASK',
+--     'EXTERNAL_EVENT',
+--     'SPAWN_THREAD',
+--     'WAIT_FOR_THREAD',
+--     'SLEEP',
+--     'NOP',
+--     'THROW_EXCEPTION'
+-- );
+
+-- CREATE TYPE lh_var_type_enum AS ENUM (
+--     'INT',
+--     'OBJECT',
+--     'ARRAY',
+--     'BOOLEAN',
+--     'DOUBLE',
+--     'STRING'
+-- );
 
 CREATE TABLE external_event_defs (
+    id VARCHAR(256) PRIMARY KEY,
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated TIMESTAMP,
+    name VARCHAR(256),
+    -- content_type lh_var_type_enum,
 
-)
+    spec JSON
+);
 
-CREATE TABLE nodes (
+-- CREATE TABLE nodes (
+--     id VARCHAR(256) PRIMARY KEY,
+--     created TIMESTAMP NOT NULL DEFAULT NOW(),
+--     updated TIMESTAMP,
+--     name VARCHAR(256),
+
+--     num_retries INTEGER,
+--     node_type node_type_enum,
+--     variables JSON,
+--     external_event_def_name VARCHAR(256) DEFAULT NULL,
+--     external_event_def_id VARCHAR(256) DEFAULT NULL REFERENCES external_event_defs(id),
+
+--     thread_wait_thread_id JSON,
+    
+-- );
+
+CREATE TABLE task_defs (
     id VARCHAR(256) PRIMARY KEY,
     created TIMESTAMP NOT NULL DEFAULT NOW(),
     updated TIMESTAMP,
     name VARCHAR(256),
 
-    num_retries INTEGER,
-    node_type node_type_enum,
-    variables JSON,
-    external_event_def_name VARCHAR(256) DEFAULT NULL,
-    external_event_def_id VARCHAR(256) DEFAULT NULL REFERENCES external_event_defs(id),
+    spec JSON
+);
 
-    thread_wait_thread_id JSON,
-    
+CREATE TABLE wf_runs (
+    id VARCHAR(512) PRIMARY KEY,
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated TIMESTAMP,
+
+    spec JSON
 );
---
+++ sql/backup.sql
@@ -0,0 +1,59 @@
+CREATE TABLE wf_specs (
+    id VARCHAR(256) PRIMARY KEY,
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated TIMESTAMP,
+    name VARCHAR(256),
+    entrypoint_thread_name VARCHAR(256)
+);
+
+CREATE TABLE thread_specs (
+    id VARCHAR(256) PRIMARY KEY,
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated TIMESTAMP,
+    name VARCHAR(256),
+    wf_spec_id VARCHAR(256) REFERENCES wf_specs(id),
+    entrypoint_node_name VARCHAR(256)
+);
+
+CREATE TYPE node_type_enum AS ENUM (
+    'TASK',
+    'EXTERNAL_EVENT',
+    'SPAWN_THREAD',
+    'WAIT_FOR_THREAD',
+    'SLEEP',
+    'NOP',
+    'THROW_EXCEPTION'
+);
+
+CREATE TYPE lh_var_type_enum AS ENUM (
+    'INT',
+    'OBJECT',
+    'ARRAY',
+    'BOOLEAN',
+    'DOUBLE',
+    'STRING'
+);
+
+CREATE TABLE external_event_defs (
+    id VARCHAR(256) PRIMARY KEY,
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated TIMESTAMP,
+    name VARCHAR(256),
+    content_type lh_var_type_enum
+);
+
+CREATE TABLE nodes (
+    id VARCHAR(256) PRIMARY KEY,
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated TIMESTAMP,
+    name VARCHAR(256),
+
+    num_retries INTEGER,
+    node_type node_type_enum,
+    variables JSON,
+    external_event_def_name VARCHAR(256) DEFAULT NULL,
+    external_event_def_id VARCHAR(256) DEFAULT NULL REFERENCES external_event_defs(id),
+
+    thread_wait_thread_id JSON,
+    
+);
